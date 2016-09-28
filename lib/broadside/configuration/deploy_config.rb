782b1dd420ae51ccbb8736f1f923f87d00818a42
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-09-28 03:34:35 -0400

    Style

--- lib/broadside/configuration/deploy_config.rb
+++ lib/broadside/configuration/deploy_config.rb
@@ -32,7 +32,7 @@ module Broadside
         scale: ->(target_attribute) { validate_types([Fixnum], target_attribute) },
         env_file: ->(target_attribute) { validate_types([String], target_attribute) },
         command: ->(target_attribute) { validate_types([Array, NilClass], target_attribute) },
-        predeploy_commands: ->(target_attribute) { validate_predeploy(target_attribute) },
+        predeploy_commands: ->(target_attribute) { validate_predeploy_commands(target_attribute) },
         service_config: ->(target_attribute) { validate_types([Hash, NilClass], target_attribute) },
         task_definition_config: ->(target_attribute) { validate_types([Hash, NilClass], target_attribute) }
       }
@@ -76,7 +76,6 @@ module Broadside
       # Loads deploy target data using provided target
       def load_target!
         validate_targets!
-
         env_file = Pathname.new(@targets[@target][:env_file])
 
         unless env_file.absolute?
@@ -86,13 +85,14 @@ module Broadside
 
         if env_file.exist?
           vars = Dotenv.load(env_file)
-          @env_vars = vars.map { |k,v| {'name'=> k, 'value' => v } }
+          @env_vars = vars.map { |k, v| { 'name'=> k, 'value' => v } }
         else
           raise ArgumentError, "Could not find file '#{env_file}' for loading environment variables !"
         end
 
         @scale ||= @targets[@target][:scale]
         @command = @targets[@target][:command]
+        # TODO: what's up with predeploy_commands.  ||= []?
         @predeploy_commands = @targets[@target][:predeploy_commands] if @targets[@target][:predeploy_commands]
         @service_config = @targets[@target][:service_config]
         @task_definition_config = @targets[@target][:task_definition_config]
@@ -101,18 +101,18 @@ module Broadside
       private
 
       def self.validate_types(types, target_attribute)
-        unless types.include?(target_attribute.class)
-          return "'#{target_attribute}' must be of type [#{types.join('|')}], got '#{target_attribute.class}' !"
+        if types.include?(target_attribute.class)
+          nil
+        else
+          "'#{target_attribute}' must be of type [#{types.join('|')}], got '#{target_attribute.class}' !"
         end
-
-        nil
       end
 
-      def self.validate_predeploy(target_attribute)
-        return nil if target_attribute.nil?
-        return 'predeploy_commands must be an array' unless target_attribute.is_a?(Array)
+      def self.validate_predeploy_commands(commands)
+        return nil if commands.nil?
+        return 'predeploy_commands must be an array' unless commands.is_a?(Array)
 
-        messages = target_attribute.select { |cmd| !cmd.is_a?(Array) }.map do |command|
+        messages = commands.select { |cmd| !cmd.is_a?(Array) }.map do |command|
           "predeploy_command '#{command}' must be an array" unless command.is_a?(Array)
         end
         messages.empty? ? nil : messages.join(', ')
