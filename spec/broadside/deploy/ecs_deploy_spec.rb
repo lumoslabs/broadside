498a16b4781ab1efb01e4379264c9b411aa534ca
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-10-01 13:54:59 -0400

    Clearer spec

--- spec/broadside/deploy/ecs_deploy_spec.rb
+++ spec/broadside/deploy/ecs_deploy_spec.rb
@@ -76,23 +76,9 @@ describe Broadside::EcsDeploy do
       let(:task_name) { 'TEST_APP_TEST_TARGET' }
       let(:task_definition_arn) { "arn:aws:ecs:us-east-1:1234:task-definition/#{task_name}:1" }
       let(:stub_service_response) { { services: [existing_service], failures: [] } }
-      let(:stub_describe_task_definition_response) do
-        {
-          task_definition: {
-            task_definition_arn: task_definition_arn,
-            container_definitions: [
-              {
-                name: task_name
-              }
-            ],
-            family: task_name
-          }
-        }
-      end
 
       before(:each) do
         ecs_stub.stub_responses(:describe_services, stub_service_response)
-        ecs_stub.stub_responses(:describe_task_definition, stub_describe_task_definition_response)
       end
 
       it 'fails without an existing task definition' do
@@ -101,9 +87,23 @@ describe Broadside::EcsDeploy do
 
       context 'with an existing service and task_definition' do
         let(:stub_task_definition_response) { { task_definition_arns: [task_definition_arn] } }
+        let(:stub_describe_task_definition_response) do
+          {
+            task_definition: {
+              task_definition_arn: task_definition_arn,
+              container_definitions: [
+                {
+                  name: task_name
+                }
+              ],
+              family: task_name
+            }
+          }
+        end
 
         before(:each) do
           ecs_stub.stub_responses(:list_task_definitions, stub_task_definition_response)
+          ecs_stub.stub_responses(:describe_task_definition, stub_describe_task_definition_response)
         end
 
         it 'does not fail on service issues' do
