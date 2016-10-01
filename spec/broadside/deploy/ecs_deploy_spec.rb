85288c52f96bc9c4bd686ae48de7557cf2ce7c56
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-10-01 13:53:58 -0400

    Add spec for case where there is a service but no task definition

--- spec/broadside/deploy/ecs_deploy_spec.rb
+++ spec/broadside/deploy/ecs_deploy_spec.rb
@@ -76,7 +76,6 @@ describe Broadside::EcsDeploy do
       let(:task_name) { 'TEST_APP_TEST_TARGET' }
       let(:task_definition_arn) { "arn:aws:ecs:us-east-1:1234:task-definition/#{task_name}:1" }
       let(:stub_service_response) { { services: [existing_service], failures: [] } }
-      let(:stub_task_definition_response) { { task_definition_arns: [task_definition_arn] } }
       let(:stub_describe_task_definition_response) do
         {
           task_definition: {
@@ -93,14 +92,25 @@ describe Broadside::EcsDeploy do
 
       before(:each) do
         ecs_stub.stub_responses(:describe_services, stub_service_response)
-        ecs_stub.stub_responses(:list_task_definitions, stub_task_definition_response)
         ecs_stub.stub_responses(:describe_task_definition, stub_describe_task_definition_response)
       end
 
-      it 'does not fail on service issues' do
-        pending 'need to figure out how to stub a waiter, but it gets as far as update_service'
+      it 'fails without an existing task definition' do
+        expect { deploy.deploy }.to raise_error(/No task definition for/)
+      end
 
-        expect { deploy.deploy }.to_not raise_error
+      context 'with an existing service and task_definition' do
+        let(:stub_task_definition_response) { { task_definition_arns: [task_definition_arn] } }
+
+        before(:each) do
+          ecs_stub.stub_responses(:list_task_definitions, stub_task_definition_response)
+        end
+
+        it 'does not fail on service issues' do
+          pending 'need to figure out how to stub a waiter, but it gets as far as update_service'
+
+          expect { deploy.deploy }.to_not raise_error
+        end
       end
     end
   end
