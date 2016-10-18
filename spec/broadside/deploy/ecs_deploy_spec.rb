ce48205ce45ace82f063ad570035c4a01cd5f9c1
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-10-17 20:16:32 -0400

    Thorough specs for changes to container definitions, full deploy, and service updates

--- spec/broadside/deploy/ecs_deploy_spec.rb
+++ spec/broadside/deploy/ecs_deploy_spec.rb
@@ -8,28 +8,43 @@ describe Broadside::EcsDeploy do
   let(:family) { "#{app_name}_#{task_name}" }
   let(:valid_options) { { target: task_name.to_sym } }
   let(:deploy) { described_class.new(valid_options) }
+
+  let(:api_request_log) { [] }
+
   let(:ecs_stub) do
-    Aws::ECS::Client.new(
+    requests = api_request_log
+    client = Aws::ECS::Client.new(
       region: Broadside.config.aws.region,
       credentials: Aws::Credentials.new('access', 'secret'),
       stub_responses: true
     )
+
+    client.handle do |context|
+      requests << { context.operation_name => context.params }
+      @handler.call(context)
+    end
+
+    client
   end
 
+  let(:minimum_healthy_percent) { 40 }
   let(:service_config) do
     {
+      desired_count: 2,
       deployment_configuration: {
-        minimum_healthy_percent: 0.5,
+        minimum_healthy_percent: minimum_healthy_percent,
       }
     }
   end
 
+  let(:cpu) { 1 }
+  let(:memory) { 2000 }
   let(:task_definition_config) do
     {
       container_definitions: [
         {
-          cpu: 1,
-          memory: 2000,
+          cpu: cpu,
+          memory: memory,
         }
       ]
     }
@@ -106,8 +121,27 @@ describe Broadside::EcsDeploy do
           ecs_stub.stub_responses(:describe_task_definition, stub_describe_task_definition_response)
         end
 
-        it 'does not fail on service issues' do
-          expect { deploy.deploy }.to_not raise_error
+        it 'short deploy does not fail' do
+          expect { deploy.short }.to_not raise_error
+        end
+
+        it 'should reconfigure the task definition' do
+          deploy.deploy_config.task_definition_config = task_definition_config
+          deploy.short
+
+          register_requests = api_request_log.select { |cmd| cmd.keys.first == :register_task_definition }
+          expect(register_requests.size).to eq(1)
+
+          expect(register_requests.first.values.first[:container_definitions].first[:cpu]).to eq(cpu)
+          expect(register_requests.first.values.first[:container_definitions].first[:memory]).to eq(memory)
+        end
+
+        it 'should reconfigure the service definition' do
+          deploy.deploy_config.service_config = service_config
+          deploy.short
+
+          service_requests = api_request_log.select { |cmd| cmd.keys.first == :update_service }
+          expect(service_requests.first.values.first[:desired_count]).to eq(2)
         end
       end
     end
