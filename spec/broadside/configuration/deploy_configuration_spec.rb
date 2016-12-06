eb3c74b9b2b8c3e708a1258fea5f6cc3399c06d4
Matthew Leung <leung.mattp@gmail.com>
2016-12-06 15:59:47 -0800

    Add feature to support multiple env files per target

--- spec/broadside/configuration/deploy_configuration_spec.rb
+++ spec/broadside/configuration/deploy_configuration_spec.rb
@@ -48,6 +48,63 @@ module Broadside
         include_examples 'valid_configuration?', false, command: 'bundle exec rails s'
         include_examples 'valid_configuration?', true,  command: nil
         include_examples 'valid_configuration?', true,  command: ['bundle', 'exec', 'resque:work']
+
+
+        include_examples 'valid_configuration?', true,  env_file: ['env_file1', 'env_file2']
+      end
+
+      describe '#load_env_vars!' do
+        context 'with a single environment file' do
+          let(:config) do
+            config = Broadside::Configuration::DeployConfig.new
+            config.targets = {
+              test_target: {
+                env_file: File.join(FIXTURES_PATH, 'sample_dotenv')
+              }
+            }
+            config.target = :test_target
+            config
+          end
+          let(:expected_env_vars) do
+            [{'name'=>'TEST_KEY1', 'value'=>'TEST_VALUE1'},
+             {'name'=>'TEST_KEY2', 'value'=>'TEST_VALUE2'}]
+          end
+
+          it 'loads environment variables from a file' do
+            expect(config.env_vars).to eq(nil)
+            config.load_env_vars!
+            expect(config.env_vars).to eq(expected_env_vars)
+          end
+        end
+
+        context 'with multiple environment files' do
+          let(:config) do
+            config = Broadside::Configuration::DeployConfig.new
+            config.targets = {
+              test_target: {
+                env_file: [File.join(FIXTURES_PATH, 'sample_dotenv_a'), File.join(FIXTURES_PATH, 'sample_dotenv_b')]
+              }
+            }
+            config.target = :test_target
+            config
+          end
+          let(:expected_env_vars) do
+            [
+              {'name'=>'SHARED_KEY_1', 'value'=>'SHARED_VALUE_1b'},
+              {'name'=>'SHARED_KEY_2', 'value'=>'SHARED_VALUE_2b'},
+              {'name'=>'UNIQUE_KEY_1a', 'value'=>'UNIQUE_VALUE_1a'},
+              {'name'=>'UNIQUE_KEY_2a', 'value'=>'UNIQUE_VALUE_1a'},
+              {'name'=>'UNIQUE_KEY_1b', 'value'=>'UNIQUE_VALUE_1b'},
+              {'name'=>'UNIQUE_KEY_2b', 'value'=>'UNIQUE_VALUE_1b'},
+            ]
+          end
+
+          it 'loads the last environment file with highest precedence' do
+            expect(config.env_vars).to eq(nil)
+            config.load_env_vars!
+            expect(config.env_vars).to eq(expected_env_vars)
+          end
+        end
       end
     end
   end
