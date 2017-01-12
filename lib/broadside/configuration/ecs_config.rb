41fce94b497a439a1297ba1270c958d90c10dcfe
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-01-12 13:46:17 -0500

    Add deprecation warning

--- lib/broadside/configuration/ecs_config.rb
+++ lib/broadside/configuration/ecs_config.rb
@@ -1,7 +1,16 @@
 module Broadside
   class Configuration
     class EcsConfig < ConfigStruct
+      extend Gem::Deprecate
+
       attr_accessor :cluster, :poll_frequency
+      deprecate(
+        :cluster=,
+        'You should configure the cluster on a per target basis',
+        2017,
+        4
+      )
+
 
       def initialize
         @cluster = nil
