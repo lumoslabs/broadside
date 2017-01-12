28d09549e04dc1fd8e5f3db21ccd5d237eaaf041
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-01-12 00:30:34 -0500

    Working env files specs

--- spec/configuration/target_spec.rb
+++ spec/configuration/target_spec.rb
@@ -45,40 +45,25 @@ describe Broadside::Target do
   end
 
   describe '#load_env_vars!' do
+    let(:env_files) { File.join(FIXTURES_PATH, 'sample_dotenv') }
+    let(:valid_options) { { scale: 100, env_files: env_files } }
+    let(:target) { described_class.new('tarbaby_target', valid_options) }
+
     context 'with a single environment file' do
-      let(:config) do
-        config = described_class.new
-        config.targets = {
-          test_target: {
-            env_files: File.join(FIXTURES_PATH, 'sample_dotenv')
-          }
-        }
-        config.target = :test_target
-        config
-      end
       let(:expected_env_vars) do
-        [{'name'=>'TEST_KEY1', 'value'=>'TEST_VALUE1'},
-         {'name'=>'TEST_KEY2', 'value'=>'TEST_VALUE2'}]
+        [
+          { 'name' => 'TEST_KEY1', 'value' => 'TEST_VALUE1'},
+          { 'name' => 'TEST_KEY2', 'value' => 'TEST_VALUE2'}
+        ]
       end
 
       it 'loads environment variables from a file' do
-        expect(config.env_vars).to eq(nil)
-        config.load_env_vars!
-        expect(config.env_vars).to eq(expected_env_vars)
+        expect(target.env_vars).to eq(expected_env_vars)
       end
     end
 
     context 'with multiple environment files' do
-      let(:config) do
-        config = described_class.new
-        config.targets = {
-          test_target: {
-            env_files: [File.join(FIXTURES_PATH, 'sample_dotenv_a'), File.join(FIXTURES_PATH, 'sample_dotenv_b')]
-          }
-        }
-        config.target = :test_target
-        config
-      end
+      let(:env_files) { [File.join(FIXTURES_PATH, 'sample_dotenv_a'), File.join(FIXTURES_PATH, 'sample_dotenv_b')] }
       let(:expected_env_vars) do
         [
           {'name'=>'SHARED_KEY_1', 'value'=>'SHARED_VALUE_1b'},
@@ -91,9 +76,7 @@ describe Broadside::Target do
       end
 
       it 'loads the last environment file with highest precedence' do
-        expect(config.env_vars).to eq(nil)
-        config.load_env_vars!
-        expect(config.env_vars).to eq(expected_env_vars)
+        expect(target.env_vars).to eq(expected_env_vars)
       end
     end
   end
