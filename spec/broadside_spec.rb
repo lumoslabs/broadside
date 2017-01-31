6fbcd972136e641351edfae895f2890116b11243
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-01-31 03:16:19 -0800

    remove spec cruft

--- spec/broadside_spec.rb
+++ spec/broadside_spec.rb
@@ -4,32 +4,24 @@ describe Broadside do
   describe '#load_config' do
     let(:system_config_path) { File.join(FIXTURES_PATH, 'broadside_system_example.conf.rb') }
     let(:app_config_path) { File.join(FIXTURES_PATH, 'broadside_app_example.conf.rb') }
+    let(:ssh_system_user) { { user: 'system-default-user' } }
+    let(:ssh_app_user)    { { user: 'app-default-user' } }
 
-    context 'system config loading precedence' do
-      before do
-        allow(Broadside).to receive(:load)
-        # stub out verify from erroring since load is stubbed
-        allow(Broadside.config).to receive(:verify)
-        stub_const('Broadside::USER_CONFIG_FILE', system_config_path)
-      end
-
-      it 'loads a system-level config file and passed in config file' do
-        Broadside.load_config(app_config_path)
-        expect(Broadside).to have_received(:load).with(system_config_path)
-        expect(Broadside).to have_received(:load).with(app_config_path)
-      end
+    before do
+      stub_const('Broadside::USER_CONFIG_FILE', system_config_path)
     end
 
-    context 'with a system config' do
-      let(:ssh_system_user) { { user: 'system-default-user' } }
-      let(:ssh_app_user)    { { user: 'app-default-user' } }
+    it 'calls load for both the system and app config files' do
+      expect(Broadside).to receive(:load).with(system_config_path)
+      expect(Broadside).to receive(:load).with(app_config_path)
+      expect(Broadside.config).to receive(:verify)
+      Broadside.load_config(app_config_path)
+    end
 
-      it 'loads the app-specific config with a higher precedence than the system-level config' do
-        stub_const('Broadside::USER_CONFIG_FILE', system_config_path)
-        expect(Broadside.config).to receive(:verify)
-        Broadside.load_config(app_config_path)
-        expect(Broadside.config.ssh).to eq(ssh_app_user)
-      end
+    it 'loads the app-specific config with a higher precedence than the system-level config' do
+      expect(Broadside.config).to receive(:verify)
+      Broadside.load_config(app_config_path)
+      expect(Broadside.config.ssh).to eq(ssh_app_user)
     end
   end
 
