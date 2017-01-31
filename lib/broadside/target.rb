b42119ea2f3c6b34840d768772ed567e4df148ac
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-01-31 01:03:19 -0800

    Env file validation

--- lib/broadside/target.rb
+++ lib/broadside/target.rb
@@ -36,10 +36,6 @@ module Broadside
       record.errors.add(attr, 'must be an array') unless val.is_a?(Array)
     end
 
-    validates_each :env_files, allow_nil: true do |record, attr, val|
-      record.errors.add(attr, ':env_file does not exist') unless val.all? { |env_file| env_file.exist? }
-    end
-
     def initialize(name, options = {})
       @name = name
       @config = options
@@ -49,13 +45,10 @@ module Broadside
       @command = @config[:command]
       @env_files = Array.wrap(@config[:env_files] || @config[:env_file]).map do |env_path|
         env_file = Pathname.new(env_path)
+        next env_file if env_file.absolute?
 
-        unless env_file.absolute?
-          dir = Broadside.config.config_file ? Pathname.new(config.config_file).dirname : Dir.pwd
-          env_file = env_file.expand_path(dir)
-        end
-
-        env_file
+        dir = Broadside.config.config_file ? Pathname.new(Broadside.config.config_file).dirname : Dir.pwd
+        env_file.expand_path(dir)
       end
       @predeploy_commands = @config[:predeploy_commands]
       @scale = @config[:scale]
@@ -67,11 +60,13 @@ module Broadside
 
     # Convert env files to key/value format ECS expects
     def load_env_vars!
-      @env_vars = @env_files.inject({}) do |memo, env_path|
+      @env_vars = @env_files.inject({}) do |memo, env_file|
+        raise "#{env_file} does not exist!" unless env_file.exist?
+
         begin
-          memo.merge(Dotenv.load(env_path))
+          memo.merge(Dotenv.load(env_file))
         rescue Dotenv::FormatError => e
-          raise e.class, "Dotenv gem error: '#{e.message}' while parsing #{env_path}", e.backtrace
+          raise e.class, "Error parsing #{env_file}: #{e.message}", e.backtrace
         end
       end.map { |k, v| { 'name' => k, 'value' => v } }
     end
