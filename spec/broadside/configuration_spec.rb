c5e587bf64eaaeedcedb9983634ae7c528e1882c
Matthew Leung <leung.mattp@gmail.com>
2017-02-03 13:19:25 -0800

    nested contexts are better

--- spec/broadside/configuration_spec.rb
+++ spec/broadside/configuration_spec.rb
@@ -34,50 +34,50 @@ describe Broadside::Configuration do
       Broadside.config.ssh = ssh_config
     end
 
-    it 'should build the ssh command' do
+    it 'should build the SSH command' do
       expect(Broadside.config.ssh_cmd(ip)).to eq("ssh -o StrictHostKeyChecking=no #{ip}")
     end
 
-    context 'with tty option' do
-      it 'generates an ssh command string with -tt flags' do
-        expect(Broadside.config.ssh_cmd(ip, tty: true)).to eq(
-          "ssh -o StrictHostKeyChecking=no -t -t #{ip}"
-        )
-      end
-    end
-
-    context 'with ssh user and keyfile provided in the config' do
+    context 'with configured SSH user and keyfile' do
       let(:ssh_config) { { user: user, keyfile: keyfile } }
 
-      it 'generates an ssh command string with keyfile flag and user set' do
+      it 'generates an SSH command string with keyfile flag and user set' do
         expect(Broadside.config.ssh_cmd(ip)).to eq(
           "ssh -o StrictHostKeyChecking=no -i #{keyfile} #{user}@#{ip}"
         )
       end
-    end
 
-    context 'with ssh proxy' do
-      let(:proxy_user) { 'proxy-user' }
-      let(:proxy_host) { 'proxy-host' }
-      let(:proxy_port) { '22' }
-      let(:proxy_keyfile) { 'path_to_proxy_keyfile' }
-      let(:ssh_config) do
-        {
-          user: user,
-          keyfile: keyfile,
-          proxy: {
-            user: proxy_user,
-            host: proxy_host,
-            port: proxy_port,
-            keyfile: proxy_keyfile
-          }
-        }
+      context 'with tty option' do
+        it 'generates an SSH command string with -tt flags' do
+          expect(Broadside.config.ssh_cmd(ip, tty: true)).to eq(
+            "ssh -o StrictHostKeyChecking=no -t -t -i #{keyfile} #{user}@#{ip}"
+          )
+        end
       end
 
-      it 'generates an ssh command string with the configured ssh proxy' do
-        expect(Broadside.config.ssh_cmd(ip)).to eq(
-          "ssh -o StrictHostKeyChecking=no -i #{keyfile} -o ProxyCommand=\"ssh -q -i #{proxy_keyfile} #{proxy_user}@#{proxy_host} nc #{ip} #{proxy_port}\" #{user}@#{ip}"
-        )
+      context 'with configured SSH proxy' do
+        let(:proxy_user) { 'proxy-user' }
+        let(:proxy_host) { 'proxy-host' }
+        let(:proxy_port) { '22' }
+        let(:proxy_keyfile) { 'path_to_proxy_keyfile' }
+        let(:ssh_config) do
+          {
+            user: user,
+            keyfile: keyfile,
+            proxy: {
+              user: proxy_user,
+              host: proxy_host,
+              port: proxy_port,
+              keyfile: proxy_keyfile
+            }
+          }
+        end
+
+        it 'generates an SSH command string with the configured SSH proxy' do
+          expect(Broadside.config.ssh_cmd(ip)).to eq(
+            "ssh -o StrictHostKeyChecking=no -i #{keyfile} -o ProxyCommand=\"ssh -q -i #{proxy_keyfile} #{proxy_user}@#{proxy_host} nc #{ip} #{proxy_port}\" #{user}@#{ip}"
+          )
+        end
       end
     end
   end
