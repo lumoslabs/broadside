99e0776785ecfa42254476f66b60b9798af55220
Matthew Leung <leung.mattp@gmail.com>
2017-02-03 19:59:58 -0800

    hound

--- lib/broadside/ecs/ecs_deploy.rb
+++ lib/broadside/ecs/ecs_deploy.rb
@@ -201,7 +201,7 @@ module Broadside
           task_arn = EcsManager.run_task(cluster, family, command, options).tasks[0].task_arn
           info "Launched #{command_name} task #{task_arn}, waiting for completion..."
 
-          EcsManager.ecs.wait_until(:tasks_stopped, { cluster: cluster, tasks: [task_arn] }) do |w|
+          EcsManager.ecs.wait_until(:tasks_stopped, cluster: cluster, tasks: [task_arn]) do |w|
             w.max_attempts = nil
             w.delay = Broadside.config.ecs.poll_frequency
             w.before_attempt do |attempt|
