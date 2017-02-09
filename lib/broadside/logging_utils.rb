1a0f9e2cd93bca4ae6ed458bbff9e88999ab3591
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-02-09 00:27:46 -0800

    public_send instead of send

--- lib/broadside/logging_utils.rb
+++ lib/broadside/logging_utils.rb
@@ -2,7 +2,7 @@ module Broadside
   module LoggingUtils
     %w(debug info warn error fatal).each do |log_level|
       define_method(log_level) do |*args|
-        Broadside.config.logger.send(log_level.to_sym, args.join(' '))
+        Broadside.config.logger.public_send(log_level.to_sym, args.join(' '))
       end
     end
   end
