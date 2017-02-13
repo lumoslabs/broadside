b2e44b60357dd98cc36c2f88dc85460c1db780f2
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-02-13 12:56:24 -0800

    Hound

--- lib/broadside/configuration/invalid_configuration.rb
+++ lib/broadside/configuration/invalid_configuration.rb
@@ -1,7 +1,7 @@
 module Broadside
   module InvalidConfiguration
     def method_missing(m, *args, &block)
-      message = "Unknown '#{m}' provided for #{self.is_a?(AwsConfiguration) ? 'configuration.aws' : 'configuration'}!"
+      message = "Unknown '#{m}' provided for #{is_a?(AwsConfiguration) ? 'configuration.aws' : 'configuration'}!"
       raise ArgumentError, message
     end
   end
