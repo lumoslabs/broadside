lumoslabs/broadside

commit a697d6fe982e177d5240184b007322ac396fafb1

Adding --tag to bootstrap command in GLI specification (#80)
* Adding --tag to bootstrap command in GLI specification

* Updating changelog

lib/broadside/gli/commands.rb
@@ -24,6 +24,7 @@ end
 
 desc 'Bootstrap your service and task definition from the configured definition.'
 command :bootstrap do |bootstrap|
+  add_tag_flag(run)
   add_target_flag(bootstrap)
 
   bootstrap.action do |_, options, _|

lib/broadside/version.rb
@@ -1,3 +1,3 @@
 module Broadside
-  VERSION = '3.0.2'.freeze
+  VERSION = '3.0.3'.freeze
 end

commit 878502eba6c90e41516a40126628c01e48aa6107

Fix bug introduced in https://github.com/lumoslabs/broadside/pull/80

lib/broadside/gli/commands.rb
@@ -24,7 +24,7 @@ end
 
 desc 'Bootstrap your service and task definition from the configured definition.'
 command :bootstrap do |bootstrap|
-  add_tag_flag(run)
+  add_tag_flag(bootstrap)
   add_target_flag(bootstrap)
 
   bootstrap.action do |_, options, _|