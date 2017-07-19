df1010052815cc0512a972effb40f202f2872fb2
apurvis <apurvis@users.noreply.github.com>
2017-07-19 02:00:20 -0400

    Another try at faster travis (#96)
    * Add dummy ENV variables to try to speed up travis
    
    * Add dummy ENV variables to try to speed up travis
    
    * Better way

--- lib/broadside/configuration/aws_configuration.rb
+++ lib/broadside/configuration/aws_configuration.rb
@@ -9,19 +9,20 @@ module Broadside
       raise ConfigurationError, 'credentials is not of type Aws::Credentials' unless val.is_a?(Aws::Credentials)
     end
 
+    attr_writer :credentials
     attr_accessor(
-      :credentials,
       :ecs_default_cluster,
       :ecs_poll_frequency,
       :region
     )
 
     def initialize
-      @credentials = Aws::SharedCredentials.new.credentials
-      @credentials ||= Aws::InstanceProfileCredentials.new(retries: ENV['AWS_CREDENTIAL_RETRIES'] || 5).credentials
-
       @ecs_poll_frequency = 2
       @region = 'us-east-1'
+    end
+
+    def credentials
+      @credentials ||= Aws::SharedCredentials.new.credentials || Aws::InstanceProfileCredentials.new.credentials
     end
   end
 end
